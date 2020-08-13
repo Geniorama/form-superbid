05a8c86ccdead05e574dda11d31dc48b44c53fa6
Angel Burgos <angelb@univercity.com.co>
2020-08-12 22:52:10 -0500

    Función subir archivo OK

--- controllers/upload.php
+++ controllers/upload.php
@@ -5,23 +5,28 @@ require_once "../terceros/dropbox/vendor/autoload.php";
 use Kunnu\Dropbox\Dropbox;
 use Kunnu\Dropbox\DropboxApp;
 
-$dropboxKey = "mw8jzg5r0pv579f";
-$dropboxSecret = "nheaxswzx15gupt";
-$dropboxToken = "sl.AfpftTtgZarcsTkYfo4EGSFH0AxIP2Kcj0q63u8IU_1lyQSl6yes08pwCL7lxGtWTL5HShVx6ZZKH9CY6oHoyveXrN56KQSWbjYLD1NIPhOs45Auof3e2kHxObt2eQXT3lpBuo0";
+$dropboxKey = "ydj6licisq7rbr7";
+$dropboxSecret = "mnmvh83u87lnvnv";
+$dropboxToken = "7u3ACVFv4aAAAAAAAAAAAdG0qBUBbzPhTdxIGD6bOT1QQDldHuCQqFca4tgJgtZl";
 
 
 $app = new DropboxApp($dropboxKey,$dropboxSecret,$dropboxToken);
 $dropbox = new Dropbox($app);
 
+$listFolderContents = $dropbox->listFolder("/");
+
 if (!empty($_FILES)) {
     $nombre = uniqid();
+    $documento = $_POST['documento'];
     $tempfile = $_FILES['file']['tmp_name'];
     $ext = explode(".",$_FILES['file']['name']);
     $ext = end($ext);
+    $nombrecarpeta = "/".$documento;
     $nombredropbox = "/".$nombre.".".$ext;
 
     try {
-       $file = $dropbox->simpleUpload($tempfile,$nombredropbox,['autorename' => true]);
+        //$folder = $dropbox->createFolder("/". $documento);
+        $file = $dropbox->simpleUpload($tempfile, $nombrecarpeta . $nombredropbox,['autorename' => true]);
        echo "Archivo subido";
     } catch (\exception $e) {
         print_r($e);
